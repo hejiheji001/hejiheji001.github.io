5b860701fb142191109474b351eecc1d1e1ac683
FireAwayH <hejiheji001@gmail.com>
2017-07-16 10:32:16 +0800

    Update ttmsr.js

--- ttmsr.js
+++ ttmsr.js
@@ -6,8 +6,8 @@ var notRunning = true;
 var notSubmit = true;
 var timeLeft = 1000;
 var version = "V15 测速专用 任务提交后记下秒数后立刻退出";
-window.debugTime = 60;
-window.debugCount = true
+//window.debugTime = 60;
+//window.debugCount = true
 var bannedKeys = ["P2gv+Ol0uGjoqXS6HWGovdiQ6ukyDbpv","KUyIf2VcxGzdGtvFWK7vBibfHPr68Zjt","+JNBj78KXZyrvgVLP5AC6Q/SMem7j3fd"];
 var monthlyKeys = {
 			"W+KrSOFkjnsmxd7Nq2SEtoz9+rDt+szK": "2017-08-15", 
