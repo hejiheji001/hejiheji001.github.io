f08b5180831943dd36b14e2f8a69709583579609
FireAwayH <hejiheji001@gmail.com>
2017-08-25 14:26:15 +0800

    Update msll.js

--- msll.js
+++ msll.js
@@ -1,8 +1,8 @@
 var bannedKeys = ["aXJzdHRlc3Q="];
-var myList = ["EO2017082303036389694", "EO2017082303036394725", "EO2017082303036383691","EO2017082303036383680", "EO2017082303036389676", "EO2017082303036385669", "EO2017082303036389682","EO2017082303036388657","EO2017081603036229294", "EO2017082303036394733", "EO2017082303036392738", "EO2017082303036389694", "EO2017082303036385441", "EO2017082303036394725", "EO2017081603036224153", "EO2017082303036385674", "EO2017082303036389682", "EO2017082303036387686", "EO2017082303036386672", "EO2017081603036215080", "EO2017082303036383680", "EO2017082303036389676", "EO2017082303036388593", "EO2017082303036381621", "EO2017082303036384382", "EO2017082303036385669", "EO2017082303036385594","EO2017082303036383680", "EO2017082303036389676", "EO2017082303036384651", "EO2017082303036388588", "EO2017082303036381651", "EO2017082303036382715", "EO2017082303036384450", "EO2017071903035749466", "EO2017071903035725364", "EO2017082303036380491", "EO2017082303036389678", "EO2017081603036228284", "EO2017082303036386719", "EO2017082303036385674", "EO2017082303036389682", "EO2017082303036387686", "EO2017082303036386672", "EO2017081603036215080"];
+var myList = [];
 
 var showIn = function(){
-  $("#in").append("<input type=text class=form-control id=orderId placeholder=订单号> <input type=text class=form-control id=mobile placeholder=手机号><input type=text class=form-control id=code placeholder=兑换码><textarea id=preset class=form-control placeholder='预设订单和兑换码，以便快速获取地址。格式为 订单号@兑换码@手机号 如 EO2017082303123456789@abcdefg@13588888888 一行一条数据' style='height:200px'></textarea><textarea id=result class=form-control placeholder='破解结果(更新日期2017-08-24-22:35)' style='height:200px'></textarea>");
+  $("#in").append("<input type=text class=form-control id=orderId placeholder=订单号> <input type=text class=form-control id=mobile placeholder=手机号><input type=text class=form-control id=code placeholder=兑换码><textarea id=preset class=form-control placeholder='预设订单和兑换码，以便快速获取地址。格式为 订单号@兑换码@手机号 如 EO2017082303123456789@abcdefg@13588888888 一行一条数据' style='height:200px'></textarea><textarea id=result class=form-control placeholder='破解结果(更新日期2017-08-25-14:26)' style='height:200px'></textarea>");
   $("#yql").attr("onclick", "getEnc()");
   if(localStorage.preset && localStorage.preset.length > 0){
    $("#preset").val(localStorage.preset);
@@ -86,7 +86,7 @@ var getUrl = function(data){
         extra = " 14款" + orderId;
       }
       $.get("https://pushbear.ftqq.com/sub?sendkey=751-9616f3ff7deb3cdfda6f4f547ab5b153&text=流量充值"+extra+"&desp=" + result)
-      $("#result").text(result);
+      $("#result").val(result);
       $("#yql").text("成功 点击继续获取");
       $("#mobile").val("");
       $("#code").val("");
