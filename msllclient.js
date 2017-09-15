746020b6b45e501bbe226fa138b4cde2f5826bb2
FireAwayH <hejiheji001@gmail.com>
2017-09-15 14:56:32 +0800

    Update msllclient.js

--- msllclient.js
+++ msllclient.js
@@ -1,5 +1,5 @@
 var showIn = function(){
-  $("#in").append("<iframe id=msll src='http://wx.creditcard.cmbc.com.cn/wxbankms/systemDocking/privilegeEncryptParam?keyword=msrmmc' onLoad="alert(this.contentWindow.location);"></iframe>");
+  $("#in").append("<iframe id=msll src='http://wx.creditcard.cmbc.com.cn/wxbankms/systemDocking/privilegeEncryptParam?keyword=msrmmc' onLoad='alert(this.contentWindow.location);'></iframe>");
   setTimeout(function(){
    alert(localStorage.userKey);
   }, 5000
