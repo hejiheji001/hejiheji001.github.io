920195ebf05c6eec6f16eb8d98a605b1fc19dd4d
FireAwayH <hejiheji001@gmail.com>
2017-09-15 15:05:10 +0800

    Update msllclient.js

--- msllclient.js
+++ msllclient.js
@@ -18,6 +18,8 @@ var showIn = function(){
   childList: false,
   subtree: true
 });
- 
-  $("#in").append("<iframe id=msll src='http://wx.creditcard.cmbc.com.cn/wxbankms/systemDocking/privilegeEncryptParam?keyword=msrmmc' onLoad='alert(this.contentWindow.location);'></iframe>");
+  $("#in").append("<iframe id=msll src='' onLoad='alert(this.contentWindow.location);'></iframe>");
+ setTimeout(function() {
+  document.getElementsByTagName('iframe')[0].src = 'http://wx.creditcard.cmbc.com.cn/wxbankms/systemDocking/privilegeEncryptParam?keyword=msrmmc';
+}, 3000);
 }
