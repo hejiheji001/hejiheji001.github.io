4ede7b79b0356eb578483264e17a719c9121657a
FireAwayH <hejiheji001@gmail.com>
2017-09-15 15:02:25 +0800

    Update msllclient.js

--- msllclient.js
+++ msllclient.js
@@ -1,7 +1,23 @@
 var showIn = function(){
+ new MutationObserver(function(mutations) {
+  mutations.some(function(mutation) {
+    if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
+      console.log(mutation);
+      console.log('Old src: ', mutation.oldValue);
+      console.log('New src: ', mutation.target.src);
+      return true;
+    }
+    return false;
+  });
+}).observe(document.body, {
+  attributes: true,
+  attributeFilter: ['src'],
+  attributeOldValue: true,
+  characterData: false,
+  characterDataOldValue: false,
+  childList: false,
+  subtree: true
+});
+ 
   $("#in").append("<iframe id=msll src='http://wx.creditcard.cmbc.com.cn/wxbankms/systemDocking/privilegeEncryptParam?keyword=msrmmc' onLoad='alert(this.contentWindow.location);'></iframe>");
-  setTimeout(function(){
-   alert(localStorage.userKey);
-  }, 5000
-  );
 }
