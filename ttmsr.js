14e680a718f13879b6c891a6de04ad1573911a36
FireAwayH <hejiheji001@gmail.com>
2017-09-16 02:38:54 -0500

    Update ttmsr.js

--- ttmsr.js
+++ ttmsr.js
@@ -19,7 +19,7 @@ var bannedKeys = ["mc8JMHI0ruT72Qjj+QtRapGUpErdlow7", "iQwav5NeSXemoCx8btat4PWy7
 var monthlyKeys = {
 			"W+KrSOFkjnsmxd7Nq2SEtoz9+rDt+szK": "2017-08-15", 
 			"OSwqfpUTZyPFxk9K2b8a2fCrkw7chepY": "2017-09-21",
-			"VpZ6yaSTACft0KPnQfUppPic67IEX2mu": "2017-09-17",
+			"VpZ6yaSTACft0KPnQfUppPic67IEX2mu": "2017-10-17",
 			"gU0ipPc0/+nivZhC0hIz914M4+Lk5pqB": "2017-09-24",
 			"kRTBRP0WM4TCUN9WidwKA7fPcnfruRYe": "2017-08-17",
 			"l1zgr7B9sAplSdL0/AhUy0ScQIU0P7mF": "2017-08-20", 
