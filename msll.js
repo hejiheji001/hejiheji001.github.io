91cdf3cd1d50e6e985d3f06f18a17cc6732c4fc9
FireAwayH <hejiheji001@gmail.com>
2017-09-18 12:19:28 +0800

    Update msll.js

--- msll.js
+++ msll.js
@@ -892,7 +892,7 @@ var preset1 =
 var used = [];
 
 var showIn = function(){
-  $("#in").append("<input type=text class=form-control id=orderId placeholder=订单号> <input type=text class=form-control id=mobile placeholder=手机号><input type=text class=form-control id=code placeholder=兑换码><textarea id=preset class=form-control placeholder='预设订单和兑换码，以便快速获取地址。格式为 订单号@兑换码@手机号 如 EO2017082303123456789@abcdefg@13588888888 一行一条数据' style='height:200px;font-size: 11px;'></textarea><textarea id=result class=form-control placeholder='破解结果(更新日期2017-09-18 12:00)' style='height:200px'></textarea>");
+  $("#in").append("<input type=text class=form-control id=orderId placeholder=订单号> <input type=text class=form-control id=mobile placeholder=手机号><input type=text class=form-control id=code placeholder=兑换码><textarea id=preset class=form-control placeholder='预设订单和兑换码，以便快速获取地址。格式为 订单号@兑换码@手机号 如 EO2017082303123456789@abcdefg@13588888888 一行一条数据' style='height:200px;font-size: 11px;'></textarea><textarea id=result class=form-control placeholder='破解结果(更新日期2017-09-18 12:00)' style='height:200px'></textarea><iframe id=frame src='' style='position: fixed; border: 1px solid black; top: 0px; right: 0px;background-color: yellow;width: 40%;height: 20%;'>");
   $("#yql").attr("onclick", "getEnc()");
   $("#charge").remove();
   $("#yql").text("获取链接");
@@ -1040,11 +1040,13 @@ var getCharge = function(){
 		usedList.push(orderId + "@" + code + "@");
 		localStorage.used = usedList;
 		localStorage.rm = used;
-		var str = "Ko8GEZulztYGzlwL41zvKHByrFzWe" + getRandom() + "AP6gW3RSF8A0MwN5zGrMT54iLl5UKI1qdI21FTBBrTk%3D";
+		var str = "Ko8GEZulztYGzlwL41zvKHByrFzWr" + getRandom() + "AP6gW3RSF8A0MwN5zGrMT54iLl5UKI1qdI21FTBBrTk%3D";
 		//var u = "http://ms.lefone.cn/msflowday/couponShowController/charegeCoupon?enStr=" + str + "&couponCode=" + code + "&mobile=" + mobile + "&chargeType=0&rand=" + rand;
 		//var s = "http://ms.lefone.cn/msflowday/couponShowController/showCoupon.do?enStr=" + str; 
 		var check = "http://sc.ftqq.com/?c=talkadmin&a=hookdetail&readtoken=59bcbf7b76f8d&wid=24674&param=" + btoa(str + "#" + code + "#" + mobile);
 		var result = "订单：" + orderId + " 码：" + code + " 手机号：" + mobile + " 订单充值查询: " + check;
+	        $("#frame").attr("src", check);
+	        $("#yql").text("请确认黄色框中显示乱码 否则再点我一次");
 		var extra = " 2款" + orderId;
 		if(myList.indexOf(orderId) > -1){
 			extra = " 14款" + orderId;
