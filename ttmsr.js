063eaf941af7c2f1a586e96f149a267774d1b216
FireAwayH <hejiheji001@gmail.com>
2017-09-19 21:42:16 +0800

    Update ttmsr.js

--- ttmsr.js
+++ ttmsr.js
@@ -20,7 +20,7 @@ var monthlyKeys = {
 			"W+KrSOFkjnsmxd7Nq2SEtoz9+rDt+szK": "2017-08-15", 
 			"OSwqfpUTZyPFxk9K2b8a2fCrkw7chepY": "2017-09-21",
 			"VpZ6yaSTACft0KPnQfUppPic67IEX2mu": "2017-10-17",
-			"gU0ipPc0/+nivZhC0hIz914M4+Lk5pqB": "2017-09-24",
+			"gU0ipPc0/+nivZhC0hIz914M4+Lk5pqB": "2017-10-24",
 			"kRTBRP0WM4TCUN9WidwKA7fPcnfruRYe": "2017-08-17",
 			"l1zgr7B9sAplSdL0/AhUy0ScQIU0P7mF": "2017-08-20", 
 			"F5eEur2ik9/KrStkm32lPZOK+J01TZij": "2017-07-22",
