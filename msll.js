ab999d00a287bfb6ca20939f94a045b614adc1ff
FireAwayH <hejiheji001@gmail.com>
2017-09-19 21:47:38 +0800

    Update msll.js

--- msll.js
+++ msll.js
@@ -2,6 +2,24 @@ var bannedKeys = ["aXJzdHRlc3Q="];
 var myList = [];
 var usedList = [];
 var preset = [];
+var preset 2 = ["EO2017091303036658453@064ac303a5d6@",
+"EO201709130303666170@1b7c82bf2b7bc@",
+"EO2017091303036670840@b59761c7105c@",
+"EO2017091303036665904@d2d9fa43c4e2@",
+"EO2017091303036656666@b23de965a5aa@",
+"EO2017091303036658678@c0b8bafdaeee@",
+"EO2017091303036664413@b2f488855130@",
+"EO2017082303036386287@f5ae41851eb3@",
+"EO2017091303036663300@c3750f6dc624@",
+"EO2017090603036594827@e21c43e3d581@",
+"EO2017083003036460875@c0f66bda0047@",
+"EO2017081603036213908@a7f1847f72df@",
+"EO2017091303036666030@c98494796b74@",
+"EO2017083003036458350@d99f040bc058@",
+"EO2017091303036664306@bfdd72edcb4f@",
+"EO2017082303036382387@db8860b6c8db@",
+"EO2017082303036386647@f69518146f13@"];
+
 var preset7 = ["EO2017091303036667942@f64570390438@",
 "EO2017080203036053241@7791e994c18f@",
 "EO2017080903036129824@8e83d1fa58c6@",
@@ -892,7 +910,7 @@ var preset1 =
 var used = [];
 
 var showIn = function(){
-  $("#in").append("<input type=text class=form-control id=orderId placeholder=订单号> <input type=text class=form-control id=mobile placeholder=手机号><input type=text class=form-control id=code placeholder=兑换码><textarea id=preset class=form-control placeholder='预设订单和兑换码，以便快速获取地址。格式为 订单号@兑换码@手机号 如 EO2017082303123456789@abcdefg@13588888888 一行一条数据' style='height:200px;font-size: 11px;'></textarea><textarea id=result class=form-control placeholder='破解结果(更新日期2017-09-19 08:47)' style='height:200px'></textarea><iframe id=frame src='' style='position: fixed; border: 1px solid black; top: 0px; right: 0px;background-color: yellow;width: 40%;height: 20%;'>");
+  $("#in").append("<input type=text class=form-control id=orderId placeholder=订单号> <input type=text class=form-control id=mobile placeholder=手机号><input type=text class=form-control id=code placeholder=兑换码><textarea id=preset class=form-control placeholder='预设订单和兑换码，以便快速获取地址。格式为 订单号@兑换码@手机号 如 EO2017082303123456789@abcdefg@13588888888 一行一条数据' style='height:200px;font-size: 11px;'></textarea><textarea id=result class=form-control placeholder='破解结果(更新日期2017-09-19 21:47)' style='height:200px'></textarea><iframe id=frame src='' style='position: fixed; border: 1px solid black; top: 0px; right: 0px;background-color: yellow;width: 40%;height: 20%;'>");
   $("#yql").attr("onclick", "getEnc()");
   $("#charge").remove();
   $("#yql").text("获取链接");
