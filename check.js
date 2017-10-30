958413fea6b5d688896bc7a2f52d5be2aa648ff2
FireAwayH <hejiheji001@gmail.com>
2017-10-30 17:07:56 +0800

    Update check.js

--- check.js
+++ check.js
@@ -5,6 +5,7 @@ var check = function(){
 		var update = confirm("当前版本" + version + "，最新版本" + thisV + "，点击确定下载更新包~");
 		if(update){
 			window.open("https://pan.baidu.com/s/1jIGdGLw");
+			$("#version").html("点我去更新");
 		}
 	}
 }
