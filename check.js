56c5c6c359b7991a510a42c27f2a23a5822d9661
FireAwayH <hejiheji001@gmail.com>
2017-10-30 17:04:35 +0800

    Update check.js

--- check.js
+++ check.js
@@ -1,28 +1,10 @@
-var monthlyKeys = {
-			"W+KrSOFkjnsmxd7Nq2SEtoz9+rDt+szK": "2017-08-15", 
-			"OSwqfpUTZyPFxk9K2b8a2fCrkw7chepY": "2017-09-21",
-			"VpZ6yaSTACft0KPnQfUppPic67IEX2mu": "2017-10-17",
-			"gU0ipPc0/+nivZhC0hIz914M4+Lk5pqB": "2017-10-24",
-			"kRTBRP0WM4TCUN9WidwKA7fPcnfruRYe": "2017-08-17",
-			"l1zgr7B9sAplSdL0/AhUy0ScQIU0P7mF": "2017-08-20", 
-			"F5eEur2ik9/KrStkm32lPZOK+J01TZij": "2017-07-22",
-			"gRtPhWbHhHszycNMpbYX/ZhYlz225ACu": "2017-10-23",
-			"HhoaQ5WpehEHSWHSiHHiuoQLG5g0HfiB": "2017-09-21",
-			"2oznqUaQqT/Bl/vg/oVgXXfdfxqyfyPw": "2017-08-22",
-			"WPI27dVqrrIjzt8nRX+Zn0JuvRRPs0IS": "2017-11-04"
-};
+var thisV = "V20171030";
 
-var CheckValidation = function(){
-  var end = monthlyKeys[uk];
-  var now = new Date();
-  var extra = "";
-  if(end){
-    if(now >= new Date(end)){
-      alert("试用已到期～");
-      return;
-    }else{
-      extra = " 有效期至:" + end;
-    }
-  }
-  getCountDown();
+var check = function(){
+	if(thisV !== version){
+		var update = confirm("当前版本" + version + "，最新版本" + thisV + "，点击确定下载更新包~");
+		if(update){
+			window.open("https://pan.baidu.com/s/1jIGdGLw");
+		}
+	}
 }
